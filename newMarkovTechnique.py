7fa7c843bd724c7cb72e97360047da5273a4f97a
liamge <lg1898@nyu.edu>
2016-04-14 16:28:54 -0400

    Update

--- newMarkovTechnique.py
+++ newMarkovTechnique.py
@@ -5,8 +5,8 @@ class HaikuGenerator:
 	def __init__(self, words):
 		self.d = cmudict.dict()
 		self.word_tags = pos_tag(words.split())
-		self.adjectives1 = [w for (w, t) in self.word_tags if t == 'JJ' and if syllables(w) == 3]
-		self.nouns2 = [w for (w, t) in self.word_tags if (t == 'NN' or t == 'NNS') and syllables(w) == 2]
+		self.adjectives1 = [w for (w, t) in self.word_tags if t == 'JJ' or t == 'VBG' and self.syllables(w) == 3]
+		self.nouns2 = [w for (w, t) in self.word_tags if (t == 'NN' or t == 'NNS') and self.syllables(w) == 2]
 		
 	def syllables(word):
 		return [len(list(y for y in x if y[-1].isdigit())) for x in d[word.lower()]]
